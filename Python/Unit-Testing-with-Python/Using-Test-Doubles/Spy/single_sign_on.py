926883a64e34d6650f97d9438f7d7f1e3a06ad91
Eldan Abdrashim <eldan.abdrashim@gmail.com>
2020-01-29 00:12:10 +0600

    Added Unit testing using Spy with collaborate Stub and Dummy

---
+++ Python/Unit-Testing-with-Python/Using-Test-Doubles/Spy/single_sign_on.py
@@ -0,0 +1,27 @@
+import random
+
+
+class SingleSignOnRegistry:
+
+    def register_new_session(self, credentials):
+        """Returns an instance of SSOToken if the credentials are valid"""
+        pass
+
+    def is_valid(self, token):
+        """Return True if the token is refers to a current session"""
+        pass
+
+    def unregistered(self, token):
+        """Remove the given token from current sessions"""
+        pass
+
+
+class SSOToken:
+    def __init__(self):
+        self.id = random.randrange(10000)
+
+    def __eq__(self, other):
+        return self.id == other.guid
+
+    def __repr__(self):
+        return str(self.id)
