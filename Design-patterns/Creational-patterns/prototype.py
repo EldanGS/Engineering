d72bedb0da2767d9c3b80262812237047c9ef633
Eldan <a.eldan@alem.kz>
2020-07-23 16:37:35 +0300

    day 2, refactoring.guru - Prototype pattern

---
+++ Design-patterns/Creational-patterns/prototype.py
@@ -0,0 +1,185 @@
+import copy
+
+"""
+Prototype pattern involves creating new objects by copying existing
+objects. The object whose copies are made is called the prototype. You can
+think of the prototype object as the seed object from which other objects get
+created but you might ask why would we want to create copies of objects,
+why not just create them anew? The motivations for prototype objects are as
+follows:
+
+* Sometimes creating new objects is more expensive than copying existing objects.
+
+* Imagine a class will only be loaded at runtime and you can't access its
+constructor statically. The run-time environment creates an instance of each
+dynamically loaded class automatically and registers it with a prototype
+manager. The application can request objects from the prototype manager which
+in turn can return clones of the prototype.
+
+* The number of classes in a system can be greatly reduced by varying the
+values of a cloned object from a prototypical instance.
+
+Formally, the pattern is defined as specify the kind of objects to create
+using a prototypical instance as a model and making copies of the prototype
+to create new objects.
+
+Шаблон проектирования Prototype решает проблему копирования объектов путем
+делегирования этой задачи самим объектам. Все объекты, которые можно
+копировать, должны реализовать метод clone и использовать его для получения
+точных копий самих себя.
+"""
+
+
+class SelfReferencingEntity:
+    def __init__(self):
+        self.parent = None
+
+    def set_parent(self, parent):
+        self.parent = parent
+
+
+class SomeComponent:
+    """
+    Python provides its own interface of Prototype via `copy.copy` and
+    `copy.deepcopy` functions. And any class that wants to implement custom
+    implementations have to override `__copy__` and `__deepcopy__` member
+    functions.
+    """
+
+    def __init__(self, some_int, some_list_of_objects, some_circular_ref):
+        self.some_int = some_int
+        self.some_list_of_objects = some_list_of_objects
+        self.some_circular_ref = some_circular_ref
+
+    def __copy__(self):
+        """
+        Create a shallow copy. This method will be called whenever someone calls
+        `copy.copy` with this object and the returned value is returned as the
+        new shallow copy.
+        """
+
+        # First, let's create copies of the nested objects.
+        some_list_of_objects = copy.copy(self.some_list_of_objects)
+        some_circular_ref = copy.copy(self.some_circular_ref)
+
+        # Then, let's clone the object itself, using the prepared clones of the
+        # nested objects.
+        new = self.__class__(
+            self.some_int, some_list_of_objects, some_circular_ref
+        )
+        new.__dict__.update(self.__dict__)
+
+        return new
+
+    def __deepcopy__(self, memo={}):
+        """
+        Create a deep copy. This method will be called whenever someone calls
+        `copy.deepcopy` with this object and the returned value is returned as
+        the new deep copy.
+
+        What is the use of the argument `memo`? Memo is the dictionary that is
+        used by the `deepcopy` library to prevent infinite recursive copies in
+        instances of circular references. Pass it to all the `deepcopy` calls
+        you make in the `__deepcopy__` implementation to prevent infinite
+        recursions.
+        """
+
+        # First, let's create copies of the nested objects.
+        some_list_of_objects = copy.deepcopy(self.some_list_of_objects, memo)
+        some_circular_ref = copy.deepcopy(self.some_circular_ref, memo)
+
+        # Then, let's clone the object itself, using the prepared clones of the
+        # nested objects.
+        new = self.__class__(
+            self.some_int, some_list_of_objects, some_circular_ref
+        )
+        new.__dict__.update(self.__dict__)
+
+        return new
+
+
+if __name__ == "__main__":
+
+    list_of_objects = [1, {1, 2, 3}, [1, 2, 3]]
+    circular_ref = SelfReferencingEntity()
+    component = SomeComponent(23, list_of_objects, circular_ref)
+    circular_ref.set_parent(component)
+
+    shallow_copied_component = copy.copy(component)
+
+    # Let's change the list in shallow_copied_component and see if it changes in
+    # component.
+    shallow_copied_component.some_list_of_objects.append("another object")
+    if component.some_list_of_objects[-1] == "another object":
+        print(
+            "Adding elements to `shallow_copied_component`'s "
+            "some_list_of_objects adds it to `component`'s "
+            "some_list_of_objects."
+        )
+    else:
+        print(
+            "Adding elements to `shallow_copied_component`'s "
+            "some_list_of_objects doesn't add it to `component`'s "
+            "some_list_of_objects."
+        )
+
+    # Let's change the set in the list of objects.
+    component.some_list_of_objects[1].add(4)
+    if 4 in shallow_copied_component.some_list_of_objects[1]:
+        print(
+            "Changing objects in the `component`'s some_list_of_objects "
+            "changes that object in `shallow_copied_component`'s "
+            "some_list_of_objects."
+        )
+    else:
+        print(
+            "Changing objects in the `component`'s some_list_of_objects "
+            "doesn't change that object in `shallow_copied_component`'s "
+            "some_list_of_objects."
+        )
+
+    deep_copied_component = copy.deepcopy(component)
+
+    # Let's change the list in deep_copied_component and see if it changes in
+    # component.
+    deep_copied_component.some_list_of_objects.append("another object")
+    if component.some_list_of_objects[-1] == "another object":
+        print(
+            "Adding elements to `deep_copied_component`'s "
+            "some_list_of_objects adds it to `component`'s "
+            "some_list_of_objects."
+        )
+    else:
+        print(
+            "Adding elements to `deep_copied_component`'s "
+            "some_list_of_objects doesn't add it to `component`'s "
+            "some_list_of_objects."
+        )
+
+    # Let's change the set in the list of objects.
+    component.some_list_of_objects[1].add(10)
+    if 10 in deep_copied_component.some_list_of_objects[1]:
+        print(
+            "Changing objects in the `component`'s some_list_of_objects "
+            "changes that object in `deep_copied_component`'s "
+            "some_list_of_objects."
+        )
+    else:
+        print(
+            "Changing objects in the `component`'s some_list_of_objects "
+            "doesn't change that object in `deep_copied_component`'s "
+            "some_list_of_objects."
+        )
+
+    print(
+        f"id(deep_copied_component.some_circular_ref.parent): "
+        f"{id(deep_copied_component.some_circular_ref.parent)}"
+    )
+    print(
+        f"id(deep_copied_component.some_circular_ref.parent.some_circular_ref.parent): "
+        f"{id(deep_copied_component.some_circular_ref.parent.some_circular_ref.parent)}"
+    )
+    print(
+        "^^ This shows that deepcopied objects contain same reference, they "
+        "are not cloned repeatedly."
+    )
