0c7fcd55ab3f58644fd6b6250ed6741929916dcb
Eldan <a.eldan@alem.kz>
2020-08-16 00:17:53 +0300

    aiohttp get ip request with check first completed services

---
+++ Python/Asyncio/aiohttp_get_first_ip_response.py
@@ -0,0 +1,50 @@
+import asyncio
+import time
+from dataclasses import dataclass
+from loguru import logger
+import aiohttp
+from concurrent.futures import FIRST_COMPLETED
+
+
+@dataclass
+class Service:
+    name: str
+    url: str
+    ip_attr: str
+
+
+SERVICES = (
+    Service('ipify', 'https://api.ipify.org?format=json', 'ip'),
+    Service('ip-api', 'http://ip-api.com/json', 'query')
+)
+
+
+async def fetch(session, url: str) -> dict:
+    async with session.get(url) as response:
+        return await response.json()
+
+
+async def fetch_ip(service: Service) -> str:
+    start = time.time()
+    print(f"Fetching IP from {service.name}")
+
+    async with aiohttp.ClientSession() as session:
+        response_json = await fetch(session, service.url)
+
+    ip = response_json[service.ip_attr]
+
+    return f"{service.name} finished with result: {ip}, took: {time.time() - start}"
+
+
+async def queries() -> None:
+    futures = [fetch_ip(service) for service in SERVICES]
+    done, pending = await asyncio.wait(
+        futures, return_when=FIRST_COMPLETED
+    )
+    logger.info("result: {}", done.pop().result())
+
+
+if __name__ == '__main__':
+    ioloop = asyncio.get_event_loop()
+    ioloop.run_until_complete(queries())
+    ioloop.close()
