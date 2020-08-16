0eeb963405b2fee4be826aec1415be0d93c7f826
Eldan <a.eldan@alem.kz>
2020-08-16 08:13:21 +0300

    aiohttp get ip request, tried to use pydantic.

---
+++ Python/Asyncio/aiohttp_get_your_ip_async_with_pydantic.py
@@ -0,0 +1,77 @@
+import asyncio
+from asyncio import FIRST_COMPLETED, CancelledError
+from time import time
+from loguru import logger
+
+import aiohttp
+from aiohttp import ClientSession
+from typing import Union, Optional
+from pydantic import Field, BaseModel
+from pydantic.dataclasses import dataclass
+
+
+class Service(BaseModel):
+    name: str
+    url: str
+    ip_attr: str
+    extra: Optional[str] = None
+
+
+SERVICES = (
+    Service(**{'name': 'ipify', 'url': 'https://api.ipify.org?format=json',
+               'ip_attr': 'ip'}),
+    Service(**{'name': 'ip-api', 'url': 'http://ip-api.com/json',
+               'ip_attr': 'query'}),
+)
+
+
+async def fetch(session: ClientSession, url: str) -> dict:
+    async with session.get(url) as response:
+        return await response.json()
+
+
+async def fetch_ip(service: Service) -> str:
+    start = time()
+    logger.info("Fetching IP from {!r}", service.name)
+    my_ip = "Not found"
+    try:
+        async with aiohttp.ClientSession() as session:
+            response_json = await fetch(session, service.url)
+    except CancelledError as exc:
+        logger.debug("Cancelled fetching {!r}, {}", service.name, exc)
+        return f"{service.name}s is unresponsive"
+    except Exception:
+        logger.exception("Error with {}", service)
+        return "error"
+
+    try:
+        my_ip = response_json[service.ip_attr]
+    except KeyError:
+        logger.exception("Couldn't get ip from {} using field {}",
+                         response_json, service.ip_attr)
+
+    return f"{service.name} finished with result: {my_ip}, took: {round(time() - start, 3)}"
+
+
+async def queries(timeout: Union[int, float]) -> None:
+    futures = [fetch_ip(service) for service in SERVICES]
+    done, pending = await asyncio.wait(
+        futures,
+        timeout=timeout,
+        return_when=FIRST_COMPLETED,
+    )
+
+    for future in pending:
+        future.cancel()
+
+    for future in done:
+        logger.info("result: {!r}", future.result())
+        break
+    else:
+        logger.warning("Could not fetch any result in time {}s", timeout)
+
+
+if __name__ == '__main__':
+    ioloop = asyncio.get_event_loop()
+    ioloop.run_until_complete(queries(1))
+    ioloop.close()
